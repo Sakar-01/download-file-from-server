2217619cd2fc8e036063f361304bcc0832d4ce70
Sakar <sakarvaidya72527@gmail.com>
2022-08-01 17:01:37 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const router = express.Router();
+
+router.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/index.html"));
+  //__dirname : It will resolve to your project folder.
+});
+router.get("/about", function (req, res) {
+  res.sendFile(path.join(__dirname + "/about.html"));
+  //__dirname : It will resolve to your project folder.
+});
+
+app.get('/download', function (req, res) {
+  const file = `${__dirname}/upload/pscp.exe`;
+  res.download(file); // Set disposition and send it.
+});
+
+//add the router
+app.use("/", router);
+app.listen(process.env.port || 3000);
+
+console.log("Running at Port 3000");
